0f79fecf2a1e6cb82cc92fa27683a150c0dea014
Jan Collijs <jan.collijs@inuits.eu>
2017-03-22 17:06:02 +0100

    Bugfix: wrong siteurl

--- github_publishconf.py
+++ github_publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://lns-events.github.io/lns-events.be'
+SITEURL = 'https://visibilityspots.github.io/lns-events'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
