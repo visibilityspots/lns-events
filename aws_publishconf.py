fff883c26a51ec2c7849f076779efada178b450e
Jan Collijs <github@visibilityspots.org>
2017-04-07 08:36:31 +0200

    Feature: using short url domain instead of AWS S3 one

--- aws_publishconf.py
+++ aws_publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://lns-events.be.s3-website-eu-west-1.amazonaws.com'
+SITEURL = 'http://lns-events.be'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
